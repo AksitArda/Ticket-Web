2b4c88017a9df545d4ce37006059c8bc2f5ea0e1
AksitArda <aksithuseyinarda@gmail.com>
2022-03-22 21:09:43 +0300

    Custom error handler

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signupUserRouter } from "./routes/signup";
 import { signinUserRouter } from "./routes/signin";
 import { signoutUserRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -12,6 +16,12 @@ app.use(currentUserRouter);
 app.use(signupUserRouter);
 app.use(signinUserRouter);
 app.use(signoutUserRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
