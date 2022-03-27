0153d2640cc73409acb16b0e3d06870ed26e4809
AksitArda <aksithuseyinarda@gmail.com>
2022-03-27 20:02:22 +0300

    Signin & Signout & Current-user

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,21 +2,29 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
 import { currentUserRouter } from "./routes/current-user";
-import { signupUserRouter } from "./routes/signup";
-import { signinUserRouter } from "./routes/signin";
-import { signoutUserRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
-app.use(signupUserRouter);
-app.use(signinUserRouter);
-app.use(signoutUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
@@ -25,6 +33,10 @@ app.all("*", async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("Connected to MongoDB");
--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -7,16 +7,28 @@ interface UserAttrs {
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 //An interface that describes the properties
 //that a User Model has
